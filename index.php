c576fb1847b326596cca32ac3f07037af57349d8
bishalsapkota <bishalsap2012@gmail.com>
2018-09-16 12:11:52 +0545

    Added config: added login/reg pages; added footer

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php 
+  // Allow the config
+  define('__CONFIG__', true);
+  // Required the config
+  require_once "inc/config.php"; 
+?>
+
 <!DOCTYPE html> 
 <html lang="en">
    <head>
@@ -20,36 +27,18 @@
     <body>
 
         <div class="uk-section uk-container">
-            <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
-              <form class="uk-form-stacked js-login">
+            <?php 
+              echo "Hello World, Today is: "; 
+              echo date("Y m d");
+            ?>
 
-                  <div class="uk-margin">
-                      <label class="uk-form-label" for="form-stacked-text">Email</label>
-                      <div class="uk-form-controls">
-                          <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
-                      </div>
-                  </div>
-
-                  <div class="uk-margin">
-                      <label class="uk-form-label" for="form-stacked-text">Password</label>
-                      <div class="uk-form-controls">
-                          <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="Your Password">
-                      </div>
-                  </div>
-
-                  <div class="uk-margin">
-                      <button class="uk-button uk-button-default" type="submit">Login</button>
-                  </div>
-
-              </form>
-            </div>
+            <p>
+                <a href="login.php">LOGIN</a>
+                <a href="register.php">REGISTER</a>
+            </p>
         </div>
 
-      <!-- JQuery -->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-      <!-- UIkit JS -->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.14/js/uikit.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.14/js/uikit-icons.min.js"></script>
+        <?php require_once "inc/footer.php"; ?>
 
     </body>
 
