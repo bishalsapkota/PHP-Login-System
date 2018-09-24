96c951cb9cf894ad9fba5471d78646f3dd9ec8b9
bishalsapkota <bishalsap2012@gmail.com>
2018-09-24 17:03:52 +0545

    Added Page+User class; changed functions; moved functions to classes

--- dashboard.php
+++ dashboard.php
@@ -1,40 +1,38 @@
 <?php 
+
   // Allow the config
   define('__CONFIG__', true);
-  // Required the config
+  // Require the config
   require_once "inc/config.php"; 
 
-  ForceLogin();
+  Page::ForceLogin();
+
+  $User = new User($_SESSION['user_id']);
 
 ?>
 
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html lang="en">
-   <head>
-    <meta name="description" content="Business Site - Responsive Business Template" />
-    <link rel="profile" href="http://gmpg.org/xfn/11">
-    <meta name="keywords" content="Responsive Business Template" />
-    <meta name="author" content="Promenade Themes" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta charset="utf-8">
-    <meta name="format-detection" content="telephone=no">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      
-      <title>PHP Login System</title>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="follow">
 
-    <!-- UIkit CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.14/css/uikit.min.css" />
+    <title>Page Title</title>
 
-   </head>
+    <base href="/" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css" />
+  </head>
 
-    <body>
+  <body>
 
-        <div class="uk-section uk-container">
-            Dashboard Here : You are Signed In as User: <?php echo  $_SESSION['user_id'];  ?>
-        </div>
+    <div class="uk-section uk-container">
+      <h2>Dashboard</h2>
+      <p>Hello <?php echo $User->email; ?>, you registered at <?php echo $User->reg_time; ?></p>
+      <p><a href="logout.php">Logout</a></p>
+    </div>
 
-        <?php require_once "inc/footer.php"; ?>
-
-    </body>
-
+    <?php require_once "inc/footer.php"; ?> 
+  </body>
 </html>
--- register.php
+++ register.php
@@ -1,65 +1,59 @@
 <?php 
+
   // Allow the config
   define('__CONFIG__', true);
-  // Required the config
-  require_once "inc/config.php";  
+  // Require the config
+  require_once "inc/config.php"; 
 
-  ForceDashboard();
+  Page::ForceDashboard();
 ?>
 
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html lang="en">
-   <head>
-    <meta name="description" content="Business Site - Responsive Business Template" />
-    <link rel="profile" href="http://gmpg.org/xfn/11">
-    <meta name="keywords" content="Responsive Business Template" />
-    <meta name="author" content="Promenade Themes" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta charset="utf-8">
-    <meta name="format-detection" content="telephone=no">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      
-      <title>PHP Login System</title>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="follow">
 
-    <!-- UIkit CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.14/css/uikit.min.css" />
+    <title>Page Title</title>
 
-   </head>
+    <base href="/" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css" />
+  </head>
 
-    <body>
+  <body>
 
-        <div class="uk-section uk-container">
-            <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
-              <form class="uk-form-stacked js-register">
+    <div class="uk-section uk-container">
+      <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+      <form class="uk-form-stacked js-register">
+        
+        <h2>Register</h2>
 
-                  <h2>Register</h2>
-                  
-                  <div class="uk-margin">
-                      <label class="uk-form-label" for="form-stacked-text">Email</label>
-                      <div class="uk-form-controls">
-                          <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
-                      </div>
-                  </div>
+          <div class="uk-margin">
+              <label class="uk-form-label" for="form-stacked-text">Email</label>
+              <div class="uk-form-controls">
+                  <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
+              </div>
+          </div>
 
-                  <div class="uk-margin">
-                      <label class="uk-form-label" for="form-stacked-password">Password</label>
-                      <div class="uk-form-controls">
-                          <input class="uk-input" id="form-stacked-password" type="password" required="required" placeholder="Your Password">
-                      </div>
-                  </div>
+          <div class="uk-margin">
+              <label class="uk-form-label" for="form-stacked-password">Passphrase</label>
+              <div class="uk-form-controls">
+                  <input class="uk-input" id="form-stacked-password" type="password" required='required' placeholder="Your passphrase">
+              </div>
+          </div>
 
-                  <div class="uk-margin uk-alert uk-alert-danger js-error" style="display:none;"></div>
+          <div class="uk-margin uk-alert uk-alert-danger js-error" style='display: none;'></div>
 
-                  <div class="uk-margin">
-                      <button class="uk-button uk-button-default" type="submit">Register</button>
-                  </div>
+          <div class="uk-margin">
+              <button class="uk-button uk-button-default" type="submit">Register</button>
+          </div>
 
-              </form>
-            </div>
-        </div>
+      </form>
+      </div>
+    </div>
 
-        <?php require_once "inc/footer.php"; ?>
-
-    </body>
-
+    <?php require_once "inc/footer.php"; ?> 
+  </body>
 </html>
